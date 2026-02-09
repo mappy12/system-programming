57a790423472b3b857eb5ac1bb2683146131a009
mappy12 <mappydesuu@gmail.com>
2026-02-10 00:00:02 +0400

    добавил функцию создания файла, заполненного нулями

--- src/main.cpp
+++ src/main.cpp
@@ -3,8 +3,10 @@
 #include <string.h>
 #include <unistd.h>
 #include <errno.h>
+#include <sys/types.h>
+#include <fcntl.h>
 
-int create_text_file(const chat* path, const char* content) {
+int create_text_file(const char* path, const char* content) {
 	int fd = open(path, O_CREAT | O_WRONLY | O_TRUNC, S_IRWXU);
 	if (fd == -1) return -1;
 
@@ -25,6 +27,19 @@ int create_empty_file(const char* path) {
 	int fd = open(path, O_CREAT, O_WRONLY | O_TRUNC, S_IRWXU);
 
 	if (fd == -1) {
+		return -1;
+	}
+
+	close(fd);
+	return 0;
+}
+
+int create_zero_file(const char* path, size_t size) {
+	int fd = open(path, O_CREAT | O_WRONLY, S_IRWXU);
+	if (fd == -1) return -1;
+
+	if (ftruncate(fd, (off_t)size) == -1) {
+		close(fd);
 		return -1;
 	}
 
