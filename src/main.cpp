dd7ddcfcdd7ba00e4b16051193a2a24e03007067
mappy12 <mappydesuu@gmail.com>
2026-02-13 20:23:19 +0400

    добавил создание рандомного файла произвольного размера и создал каталог, приведенный в примере

--- src/main.cpp
+++ src/main.cpp
@@ -24,7 +24,7 @@ int create_text_file(const char* path, const char* content) {
 }
 
 int create_empty_file(const char* path) {
-	int fd = open(path, O_CREAT, O_WRONLY | O_TRUNC, S_IRWXU);
+	int fd = open(path, O_CREAT | O_WRONLY | O_TRUNC, S_IRWXU);
 
 	if (fd == -1) {
 		return -1;
@@ -45,4 +45,65 @@ int create_zero_file(const char* path, size_t size) {
 
 	close(fd);
 	return 0;
+}
+
+int create_random_file(const char* path, size_t size) {
+	int fd_to = open(path, O_CREAT | O_WRONLY | O_TRUNC, S_IRWXU);
+
+	if (fd_to == -1) {
+		return -1;
+	}
+
+	int fd_rand = open("/dev/urandom", O_RDONLY);
+	if (fd_rand == -1) {
+		close(fd_to);
+		return -1;
+	}
+
+	unsigned char buffer[4096];
+	size_t remaining_size = size;
+
+	while (remaining_size > 0) {
+		size_t to_read = (remaining_size < sizeof(buffer))
+		? remaining_size
+		: sizeof(buffer);
+
+		ssize_t actually_read = read(fd_rand, buffer, to_read);
+
+		if (actually_read <= 0) {
+			close(fd_rand);
+			close(fd_to);
+			return -1;
+		}
+
+		if (write(fd_to, buffer, actually_read) != actually_read) {
+			close(fd_rand);
+			close(fd_to);
+			return -1;
+		}
+
+		remaining_size -= actually_read;
+	}
+
+	close(fd_rand);
+	close(fd_to);
+
+	return 0;
+}
+
+int main() {
+	mkdir("../a", S_IRWXU);
+	mkdir("../a/b", S_IRWXU);
+
+	create_text_file("../a/b/cat.txt", "catts");
+	mkdir("../a/c", S_IRWXU);
+	create_empty_file("../a/c/dog.txt");
+
+	symlink("../a/c", "../a/d");
+
+	mkdir("../a/e", S_IRWXU);
+	link("../a/b/cat.txt", "../a/e/f.txt");
+
+	create_random_file("../a/e/g.bin", 500);
+	create_zero_file("../a/e/h.bin", 35);
 }
