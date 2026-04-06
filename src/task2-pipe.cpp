cf8ec929aea5608a5e3fd7fd3b5a355b062d4864
mappy12 <mappydesuu@gmail.com>
2026-04-06 23:11:02 +0400

    неименованный канал и очередь

--- src/task2-pipe.cpp
+++ src/task2-pipe.cpp
@@ -1,103 +1,134 @@
+#include "check.hpp"
 #include <iostream>
-#include <unistd.h>
-#include <sys/wait.h>
 #include <cstdlib>
 #include <ctime>
+#include <cerrno>
+#include <unistd.h>
+#include <sys/wait.h>
 #include <csignal>
 
 using namespace std;
 
-volatile sig_atomic_t child_exited = 0;
+volatile sig_atomic_t child_dead = 0;
 
-void handle_sigchld(int) { child_exited = 1; }
+void sigchld_handler(int) {
+    child_dead = 1;
+}
 
-int random_guess(int N) { return rand() % N + 1; }
+bool is_alive(pid_t pid) {
+    if (pid <= 0) return false;
+    if (kill(pid, 0) == 0) return true;
+    if (errno == EPERM) return true;
+    return false;
+}
+
+bool write_int(int fd, int value, pid_t peer) {
+    while (true) {
+        if (!is_alive(peer)) return false;
+        ssize_t n = write(fd, &value, sizeof(value));
+        if (n == sizeof(value)) return true;
+        if (n == -1 && errno == EINTR) continue;
+        if (n == -1 && errno == EPIPE) return false;
+        check(n);
+    }
+}
+
+bool read_int(int fd, int &value, pid_t peer) {
+    while (true) {
+        if (!is_alive(peer)) return false;
+        ssize_t n = read(fd, &value, sizeof(value));
+        if (n == sizeof(value)) return true;
+        if (n == 0) return false;
+        if (n == -1 && errno == EINTR) continue;
+        check(n);
+    }
+}
+
+void riddler(int secret, int read_fd, int write_fd, pid_t peer, int round) {
+    cout << "Раунд " << round << ". PID " << getpid() << " загадал число " << secret << endl;
+    fflush(stdout);
+    while (is_alive(peer)) {
+        int guess;
+        if (!read_int(read_fd, guess, peer)) break;
+        cout << "Раунд " << round << ". PID " << getpid() << " получил: " << guess << "\n";
+        fflush(stdout);
+        int correct = (guess == secret);
+        if (!write_int(write_fd, correct, peer)) break;
+        if (correct) break;
+    }
+}
+
+void guesser(int N, int write_fd, int read_fd, pid_t peer, int round) {
+    int attempts = 0;
+    while (is_alive(peer)) {
+        int guess = rand() % N + 1;
+        attempts++;
+        cout << "Раунд " << round << ". PID " << getpid() << " отправил: " << guess << "\n";
+        fflush(stdout);
+        if (!write_int(write_fd, guess, peer)) break;
+        int response;
+        if (!read_int(read_fd, response, peer)) break;
+        if (response) {
+            cout << "Раунд " << round << ". PID " << getpid()
+                 << " угадал число " << guess
+                 << " за " << attempts << " попыток.\n";
+            fflush(stdout);
+            break;
+        }
+    }
+}
+
+void play_game(bool is_parent, int read_fd, int write_fd, int N, int rounds, pid_t peer) {
+    for (int r = 1; r <= rounds && is_alive(peer); ++r) {
+        bool i_am_riddler = (r % 2 == (is_parent ? 1 : 0));
+        int secret = rand() % N + 1;
+        if (i_am_riddler)
+            riddler(secret, read_fd, write_fd, peer, r);
+        else
+            guesser(N, write_fd, read_fd, peer, r);
+    }
+}
 
 int main(int argc, char* argv[]) {
-    if(argc < 3) { 
-        cerr << "Usage: " << argv[0] << " N rounds\n"; 
-        return 1; 
+    if (argc < 3) {
+        cerr << "Usage: " << argv[0] << " N rounds\n";
+        return 1;
     }
 
     int N = atoi(argv[1]);
-    int ROUNDS = atoi(argv[2]);
+    int rounds = atoi(argv[2]);
+    srand(time(nullptr) ^ getpid());
+    signal(SIGPIPE, SIG_IGN);
 
-    if(N <= 0 || ROUNDS <= 0) { 
-        cerr << "N and rounds must be > 0\n"; 
-        return 1; 
+    int pipe_pc[2]; check(pipe(pipe_pc));
+    int pipe_cp[2]; check(pipe(pipe_cp));
+
+    struct sigaction sa{};
+    sa.sa_handler = sigchld_handler;
+    sigaction(SIGCHLD, &sa, nullptr);
+
+    pid_t pid = check(fork());
+    if (pid == 0) {
+        pid_t parent_id = getppid();
+        check(close(pipe_pc[1]));
+        check(close(pipe_cp[0]));
+        int read_fd = pipe_pc[0];
+        int write_fd = pipe_cp[1];
+        play_game(false, read_fd, write_fd, N, rounds, parent_id);
+        check(close(read_fd));
+        check(close(write_fd));
+        cout << "Ребёнок завершен\n";
+        return 0;
     }
 
-    signal(SIGCHLD, handle_sigchld);
-
-    bool parent_guesses = false; // чередование ролей
-
-    for(int round = 1; round <= ROUNDS; round++) {
-        cout << "\n=== Round " << round << " ===\n";
-
-        int fd[2];
-        if(pipe(fd) == -1) { perror("pipe"); return 1; }
-
-        pid_t pid = fork();
-        if(pid < 0) { perror("fork"); return 1; }
-
-        if(pid == 0) {
-            // CHILD
-            srand(time(nullptr) ^ getpid());
-            close(fd[1]);
-
-            int secret;
-            if(read(fd[0], &secret, sizeof(secret)) <= 0) { 
-                cerr << "Child failed to read\n"; 
-                exit(1); 
-            }
-
-            int attempts = 0;
-            if(parent_guesses) {
-                // Ребёнок загадывает, Parent угадывает
-                cout << "Child: I chose the number " << secret << endl;
-                int guess;
-                do {
-                    guess = random_guess(N);
-                    attempts++;
-                    cout << "Parent guesses: " << guess << endl;
-                } while(guess != secret);
-                cout << "Parent guessed the number in " << attempts << " attempts.\n";
-            } else {
-                // Ребёнок угадывает
-                cout << "Child: I need to guess a number between 1 and " << N << endl;
-                int guess;
-                do {
-                    guess = random_guess(N);
-                    attempts++;
-                    cout << "Child guesses: " << guess << endl;
-                } while(guess != secret);
-                cout << "Child guessed the number " << secret 
-                     << " in " << attempts << " attempts.\n";
-            }
-
-            close(fd[0]);
-            exit(0);
-        } else {
-            // PARENT
-            srand(time(nullptr) ^ getpid());
-            close(fd[0]);
-
-            int secret = rand() % N + 1;
-            if(write(fd[1], &secret, sizeof(secret)) <= 0) 
-                cerr << "Parent failed to write\n";
-            close(fd[1]);
-
-            while(!child_exited) {
-                pid_t w = waitpid(pid, nullptr, WNOHANG);
-                if(w == pid) break;
-            }
-
-            child_exited = 0;
-            parent_guesses = !parent_guesses;
-            cout << "Parent: Round finished.\n";
-        }
-    }
-
-    cout << "\n=== Game Over ===\n";
+    check(close(pipe_pc[0]));
+    check(close(pipe_cp[1]));
+    int read_fd = pipe_cp[0];
+    int write_fd = pipe_pc[1];
+    play_game(true, read_fd, write_fd, N, rounds, pid);
+    check(close(read_fd));
+    check(close(write_fd));
+    waitpid(pid, nullptr, 0);
+    cout << "Родитель завершен\n";
     return 0;
 }
--- src/task3-queue.cpp
+++ src/task3-queue.cpp
@@ -1,109 +1,165 @@
+#include "check.hpp"
 #include <iostream>
-#include <sys/ipc.h>
-#include <sys/msg.h>
-#include <sys/wait.h>
-#include <unistd.h>
 #include <cstdlib>
 #include <ctime>
+#include <cerrno>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <mqueue.h>
+#include <fcntl.h>
+#include <sys/stat.h>
+#include <csignal>
 
 using namespace std;
 
-struct Message {
-    long mtype;
-    int value;
-};
+const char* MQ_PARENT = "/lab2_mq_parent";
+const char* MQ_CHILD  = "/lab2_mq_child";
 
-int random_guess(int N) {
-    return rand() % N + 1;
+volatile sig_atomic_t child_dead = 0;
+
+void sigchld_handler(int) {
+    child_dead = 1;
+}
+
+// Проверка жив ли процесс
+bool is_alive(pid_t pid) {
+    if (pid <= 0) return false;
+    if (kill(pid, 0) == 0) return true;
+    if (errno == EPERM) return true;
+    return false;
+}
+
+bool mq_send_int(mqd_t mq, int value, pid_t peer) {
+    while (true) {
+        if (!is_alive(peer)) return false;
+
+        int res = mq_send(mq, (const char*)&value, sizeof(value), 0);
+        if (res == 0) return true;            
+        if (errno == EAGAIN) continue;        
+        check(res);                           
+    }
+}
+
+bool mq_receive_int(mqd_t mq, int& value, pid_t peer) {
+    while (true) {
+        if (!is_alive(peer)) return false;
+
+        ssize_t n = mq_receive(mq, (char*)&value, sizeof(value), nullptr);
+        if (n == sizeof(value)) return true; 
+        if (errno == EAGAIN) continue;  
+        check(n);
+    }
+}
+
+void riddler(int secret, mqd_t read_mq, mqd_t write_mq, pid_t peer, int round) {
+    cout << "Раунд " << round << ". PID " << getpid() << " загадал число.\n";
+    fflush(stdout);
+
+    while (is_alive(peer)) {
+        int guess;
+        if (!mq_receive_int(read_mq, guess, peer)) break;
+
+        cout << "Раунд " << round << ". PID " << getpid() << " получил: " << guess << "\n";
+        fflush(stdout);
+
+        int correct = (guess == secret);
+        if (!mq_send_int(write_mq, correct, peer)) break;
+        if (correct) break;
+    }
+}
+
+void guesser(int N, mqd_t write_mq, mqd_t read_mq, pid_t peer, int round) {
+    int attempts = 0;
+
+    while (is_alive(peer)) {
+        int guess = rand() % N + 1;
+        attempts++;
+
+        cout << "Раунд " << round << ". PID " << getpid() << " отправил: " << guess << "\n";
+        fflush(stdout);
+
+        if (!mq_send_int(write_mq, guess, peer)) break;
+
+        int response;
+        if (!mq_receive_int(read_mq, response, peer)) break;
+
+        if (response) {
+            cout << "Раунд " << round << ". PID " << getpid()
+                 << " угадал число " << guess
+                 << " за " << attempts << " попыток.\n";
+            fflush(stdout);
+            break;
+        }
+    }
+}
+
+void play_game(bool is_parent, mqd_t read_mq, mqd_t write_mq, int N, int rounds, pid_t peer) {
+    for (int r = 1; r <= rounds && is_alive(peer); ++r) {
+        bool i_am_riddler = (r % 2 == (is_parent ? 1 : 0));
+        int secret = rand() % N + 1;
+
+        if (i_am_riddler)
+            riddler(secret, read_mq, write_mq, peer, r);
+        else
+            guesser(N, write_mq, read_mq, peer, r);
+    }
 }
 
 int main(int argc, char* argv[]) {
     if (argc < 3) {
-        cerr << "Использование: " << argv[0] << " N rounds\n";
+        cerr << "Usage: " << argv[0] << " N rounds\n";
         return 1;
     }
 
     int N = atoi(argv[1]);
-    int ROUNDS = atoi(argv[2]);
-
-    if (N <= 0 || ROUNDS <= 0) {
-        cerr << "N и rounds должны быть > 0\n";
-        return 1;
-    }
+    int rounds = atoi(argv[2]);
 
     srand(time(nullptr) ^ getpid());
 
-    int msgid = msgget(IPC_PRIVATE, 0666 | IPC_CREAT);
-    if (msgid < 0) {
-        perror("msgget");
-        return 1;
+    struct mq_attr attr{};
+    attr.mq_maxmsg = 10;
+    attr.mq_msgsize = sizeof(int);
+
+    mq_unlink(MQ_PARENT);
+    mq_unlink(MQ_CHILD);
+
+    mqd_t mq_parent = check(mq_open(MQ_PARENT, O_CREAT | O_RDWR | O_NONBLOCK, 0666, &attr));
+    mqd_t mq_child  = check(mq_open(MQ_CHILD,  O_CREAT | O_RDWR | O_NONBLOCK, 0666, &attr));
+
+    struct sigaction sa{};
+    sa.sa_handler = sigchld_handler;
+    sigaction(SIGCHLD, &sa, nullptr);
+
+    pid_t pid = check(fork());
+    if (pid == 0) {
+        // CHILD
+        pid_t parent_id = getppid();
+        mqd_t read_mq  = check(mq_open(MQ_PARENT, O_RDONLY | O_NONBLOCK));
+        mqd_t write_mq = check(mq_open(MQ_CHILD,  O_WRONLY | O_NONBLOCK));
+
+        play_game(false, read_mq, write_mq, N, rounds, parent_id);
+
+        check(mq_close(read_mq));
+        check(mq_close(write_mq));
+
+        cout << "Ребенок завершен\n";
+        return 0;
     }
 
-    bool parent_guesses = false;
+    // PARENT
+    mqd_t read_mq  = check(mq_open(MQ_CHILD,  O_RDONLY | O_NONBLOCK));
+    mqd_t write_mq = check(mq_open(MQ_PARENT, O_WRONLY | O_NONBLOCK));
 
-    for (int round = 1; round <= ROUNDS; round++) {
-        cout << "\n=== Раунд " << round << " ===\n";
+    play_game(true, read_mq, write_mq, N, rounds, pid);
 
-        pid_t pid = fork();
-        if (pid < 0) {
-            perror("fork");
-            return 1;
-        }
+    check(mq_close(read_mq));
+    check(mq_close(write_mq));
 
-        if (pid == 0) {
-            srand(time(nullptr) ^ getpid());
+    waitpid(pid, nullptr, 0);
 
-            Message msg;
-            if (msgrcv(msgid, &msg, sizeof(int), 1, 0) < 0) {
-                perror("msgrcv");
-                exit(1);
-            }
+    mq_unlink(MQ_PARENT);
+    mq_unlink(MQ_CHILD);
 
-            int secret = msg.value;
-            int attempts = 0;
-
-            if (parent_guesses) {
-                cout << "Ребёнок: Я загадал число " << secret << endl;
-                int guess;
-                do {
-                    guess = random_guess(N);
-                    attempts++;
-                    cout << "Родитель пытается: " << guess << endl;
-                } while (guess != secret);
-                cout << "Родитель угадал число за " << attempts << " попыток.\n";
-            } else {
-                cout << "Ребёнок: Мне нужно угадать число от 1 до " << N << endl;
-                int guess;
-                do {
-                    guess = random_guess(N);
-                    attempts++;
-                    cout << "Ребёнок пробует: " << guess << endl;
-                } while (guess != secret);
-                cout << "Ребёнок угадал число " << secret << " за " << attempts << " попыток.\n";
-            }
-
-            exit(0);
-        } else {
-            int secret = rand() % N + 1;
-
-            Message msg;
-            msg.mtype = 1;
-            msg.value = secret;
-
-            if (msgsnd(msgid, &msg, sizeof(int), 0) < 0) {
-                perror("msgsnd");
-                return 1;
-            }
-
-            waitpid(pid, nullptr, 0);
-
-            parent_guesses = !parent_guesses;
-            cout << "Родитель: Раунд завершён.\n";
-        }
-    }
-
-    msgctl(msgid, IPC_RMID, nullptr);
-
-    cout << "\n=== Игра окончена ===\n";
+    cout << "Родитель завершен\n";
     return 0;
 }
