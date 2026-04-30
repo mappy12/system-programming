da980fe1bfd961c6fa6410d295f417dc82db0dd6
mappy12 <mappydesuu@gmail.com>
2026-04-30 15:16:06 +0400

    client.cpp and server.cpp

--- client.cpp
+++ client.cpp
@@ -1,3 +1,83 @@
-//
-// Created by mappy on 24.04.2026.
-//
+#include "common.h"
+#include <string>
+#include <iostream>
+#include <cstring>
+#include <netdb.h>
+
+int main(int argc, char* argv[]) {
+    if (argc < 2) {
+        std::cerr << "Usage: ./client <server_address> [port]" << std::endl;
+        return 1;
+    }
+
+    std::string host = argv[1];
+    unsigned short port = SERVER_PORT;
+    if (argc > 2) {
+        port = std::stoi(argv[2]);
+    }
+
+    int sock_fd = check(make_socket(SOCKET_TYPE));
+
+    sockaddr_in dest_address{};
+    dest_address.sin_family = AF_INET;
+    dest_address.sin_port = htons(port);
+
+    if (inet_pton(AF_INET, host.c_str(), &dest_address.sin_addr) <= 0) {
+        struct hostent* he = gethostbyname(host.c_str());
+        if (he == NULL) {
+            herror("gethostbyname");
+            close(sock_fd);
+            return 1;
+        }
+        memcpy(&dest_address.sin_addr, he->h_addr_list[0], he->h_length);
+    }
+
+    std::cout << "Connecting to " << dest_address << "..." << std::endl;
+    check(connect(sock_fd, (sockaddr*)&dest_address, sizeof(dest_address)));
+    std::cout << "Connected. Enter numbers (1-100) to guess. Type 'q' to quit." << std::endl;
+
+    char recv_buffer[64];
+    std::string message;
+
+    while (true) {
+        std::cout << "> ";
+        if (!std::getline(std::cin, message)) {
+            break;
+        }
+
+        if (message == "q" || message == "exit") {
+            break;
+        }
+
+        std::string send_msg = message + "\n";
+        if (send(sock_fd, send_msg.c_str(), send_msg.size(), 0) == -1) {
+            perror("send");
+            break;
+        }
+
+        int size = recv(sock_fd, recv_buffer, sizeof(recv_buffer) - 1, 0);
+        
+        if (size <= 0) {
+            if (size == 0) {
+                std::cout << "Server disconnected gracefully." << std::endl;
+            } else {
+                perror("recv");
+            }
+            break;
+        }
+        
+        recv_buffer[size] = '\0';
+
+        std::cout << "Server: " << recv_buffer;
+
+        if (std::string(recv_buffer).find("Correct") != std::string::npos) {
+            std::cout << "Game Over. Good job!" << std::endl;
+            break;
+        }
+    }
+
+    shutdown(sock_fd, SHUT_RDWR);
+    close(sock_fd);
+
+    return 0;
+}
--- server.cpp
+++ server.cpp
@@ -1,3 +1,148 @@
-//
-// Created by mappy on 24.04.2026.
-//
+#include "common.h"
+#include <sys/wait.h>
+#include <signal.h>
+#include <cstdlib>
+#include <ctime>
+#include <string>
+#include <sstream>
+#include <pthread.h>
+#include <sys/mman.h>
+
+pthread_mutex_t* log_mutex = nullptr;
+
+void log(const sockaddr_in& addr, const std::string& msg) {
+    pthread_mutex_lock(log_mutex);
+    std::cout << addr << ": " << msg << std::endl;
+    pthread_mutex_unlock(log_mutex);
+}
+
+void sigchld_handler(int s) {
+    while(waitpid(-1, NULL, WNOHANG) > 0);
+}
+
+void handle_game(int conn_fd, sockaddr_in client_addr) {
+    srand(time(NULL) ^ getpid());
+    int target = rand() % 100 + 1;
+    
+    char buffer[64];
+    bool game_active = true;
+
+    log(client_addr, "Game started. Guess number 1-100.");
+
+    while (game_active) {
+        int size = recv(conn_fd, buffer, sizeof(buffer) - 1, 0);
+        
+        if (size <= 0) {
+            log(client_addr, "Disconnected (recv error or close).");
+            break;
+        }
+        
+        buffer[size] = '\0';
+        std::string guess_str(buffer);
+
+        while (!guess_str.empty() && (guess_str.back() == '\n' || guess_str.back() == '\r')) {
+            guess_str.pop_back();
+        }
+
+        if (guess_str.empty()) continue;
+
+        try {
+            int guess = std::stoi(guess_str);
+            std::string response;
+
+            if (guess == target) {
+                response = "Correct! You won!\n";
+                log(client_addr, "Guessed correctly: " + guess_str);
+                game_active = false;
+            } else if (guess < target) {
+                response = "Higher\n";
+                log(client_addr, "Guess: " + guess_str + " -> Higher");
+            } else {
+                response = "Lower\n";
+                log(client_addr, "Guess: " + guess_str + " -> Lower");
+            }
+
+            send(conn_fd, response.c_str(), response.size(), 0);
+
+        } catch (...) {
+            std::string err = "Invalid input. Send a number.\n";
+            send(conn_fd, err.c_str(), err.size(), 0);
+            log(client_addr, "Invalid input: " + guess_str);
+        }
+    }
+    
+    close(conn_fd);
+    exit(0);
+}
+
+int main(int argc, char* argv[]) {
+    unsigned short port = SERVER_PORT;
+    if (argc > 1) {
+        port = std::stoi(argv[1]);
+    }
+
+    log_mutex = static_cast<pthread_mutex_t*>(mmap(NULL, sizeof(pthread_mutex_t),
+                                                   PROT_READ | PROT_WRITE,
+                                                   MAP_SHARED | MAP_ANONYMOUS, -1, 0));
+
+    if (log_mutex == MAP_FAILED) {
+        perror("mmap failed");
+        return 1;
+    }
+    
+    pthread_mutexattr_t attr;
+    pthread_mutexattr_init(&attr);
+
+    pthread_mutexattr_setpshared(&attr, PTHREAD_PROCESS_SHARED);
+    pthread_mutex_init(log_mutex, &attr);
+    pthread_mutexattr_destroy(&attr);
+
+    struct sigaction sa;
+    sa.sa_handler = sigchld_handler;
+    sigemptyset(&sa.sa_mask);
+    sa.sa_flags = SA_RESTART;
+    if (sigaction(SIGCHLD, &sa, NULL) == -1) {
+        perror("sigaction failed");
+        return 1;
+    }
+
+    auto server_address = local_addr(port); 
+    
+    int listening_socket = check(make_socket(SOCKET_TYPE));
+    check(bind(listening_socket, (sockaddr*)&server_address, sizeof(server_address)));
+    check(listen(listening_socket, 10)); // Backlog 10
+
+    std::cout << "Server started on port " << port << " (PID: " << getpid() << ")" << std::endl;
+
+    while (true) {
+        sockaddr_in connected_address{};
+        socklen_t addrlen = sizeof(connected_address);
+
+        int connected_socket = accept(listening_socket, (sockaddr*)&connected_address, &addrlen);
+        
+        if (connected_socket == -1) {
+            if (errno == EINTR) continue;
+            perror("accept");
+            continue;
+        }
+
+        pid_t pid = fork();
+        
+        if (pid < 0) {
+            perror("fork");
+            close(connected_socket);
+        } 
+        else if (pid == 0) {
+            close(listening_socket);
+            handle_game(connected_socket, connected_address);
+        } 
+        else {
+            close(connected_socket);
+        }
+    }
+
+    close(listening_socket);
+    pthread_mutex_destroy(log_mutex);
+    munmap(log_mutex, sizeof(pthread_mutex_t));
+    return 0;
+}
