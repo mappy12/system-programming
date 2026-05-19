6c99ad0b15c565f9d1046499ec9ec17543b4ab8d
mappy12 <mappydesuu@gmail.com>
2026-05-19 13:41:38 +0400

    по идеи финал до сдачи

--- client.cpp
+++ client.cpp
@@ -1,17 +1,22 @@
 #include "common.h"
+#include <signal.h>
 #include <string>
 #include <iostream>
 #include <cstring>
-#include <netdb.h>
 
 int main(int argc, char* argv[]) {
+
+    ignore_sigpipe();
+
     if (argc < 2) {
-        std::cerr << "Usage: ./client <server_address> [port]" << std::endl;
+        std::cerr << "Usage: ./client <server_ip> [port]" << std::endl;
         return 1;
     }
 
     std::string host = argv[1];
+
     unsigned short port = SERVER_PORT;
+
     if (argc > 2) {
         port = std::stoi(argv[2]);
     }
@@ -19,60 +24,118 @@ int main(int argc, char* argv[]) {
     int sock_fd = check(make_socket(SOCKET_TYPE));
 
     sockaddr_in dest_address{};
+
     dest_address.sin_family = AF_INET;
     dest_address.sin_port = htons(port);
 
-    if (inet_pton(AF_INET, host.c_str(), &dest_address.sin_addr) <= 0) {
+    if (inet_pton(AF_INET,
+                  host.c_str(),
+                  &dest_address.sin_addr) <= 0) {
+
         std::cerr << "Invalid IP address: " << host << std::endl;
+
         close(sock_fd);
+
         return 1;
     }
 
-    std::cout << "Connecting to " << dest_address << "..." << std::endl;
-    check(connect(sock_fd, (sockaddr*)&dest_address, sizeof(dest_address)));
-    std::cout << "Connected. Enter numbers (1-100) to guess. Type 'q' to quit." << std::endl;
+    std::cout << "Connecting to "
+              << dest_address
+              << "..."
+              << std::endl;
 
-    char recv_buffer[64];
-    std::string message;
+    check(connect(sock_fd,
+                  (sockaddr*)&dest_address,
+                  sizeof(dest_address)));
+
+    std::cout << "Connected."
+              << std::endl;
+
+    std::string input;
 
     while (true) {
+
         std::cout << "> ";
-        if (!std::getline(std::cin, message)) {
+
+        if (!std::getline(std::cin, input)) {
             break;
         }
 
-        if (message == "q" || message == "exit") {
+        if (input == "q" || input == "exit") {
             break;
         }
 
-        std::string send_msg = message + "\n";
-        if (send(sock_fd, send_msg.c_str(), send_msg.size(), 0) == -1) {
+        int32_t guess;
+
+        try {
+            guess = std::stoi(input);
+        }
+        catch (...) {
+            std::cout << "Invalid number." << std::endl;
+            continue;
+        }
+
+        int32_t network_guess = htonl(guess);
+
+        int sent = send(sock_fd,
+                        &network_guess,
+                        sizeof(network_guess),
+                        0);
+
+        if (sent == -1) {
             perror("send");
             break;
         }
 
-        int size = recv(sock_fd, recv_buffer, sizeof(recv_buffer) - 1, 0);
-        
+        int32_t response;
+
+        int size = recv(sock_fd,
+                        &response,
+                        sizeof(response),
+                        MSG_WAITALL);
+
         if (size <= 0) {
+
             if (size == 0) {
-                std::cout << "Server disconnected gracefully." << std::endl;
-            } else {
+                std::cout << "Server disconnected."
+                          << std::endl;
+            }
+            else {
                 perror("recv");
             }
+
             break;
         }
-        
-        recv_buffer[size] = '\0';
 
-        std::cout << "Server: " << recv_buffer;
+        response = ntohl(response);
 
-        if (std::string(recv_buffer).find("Correct") != std::string::npos) {
-            std::cout << "Game Over!" << std::endl;
-            break;
+        switch (response) {
+
+            case LOWER:
+                std::cout << "Server: Lower" << std::endl;
+                break;
+
+            case HIGHER:
+                std::cout << "Server: Higher" << std::endl;
+                break;
+
+            case CORRECT:
+                std::cout << "Server: Correct! You won!"
+                          << std::endl;
+
+                shutdown(sock_fd, SHUT_RDWR);
+                close(sock_fd);
+
+                return 0;
+
+            default:
+                std::cout << "Server: Unknown response"
+                          << std::endl;
         }
     }
 
     shutdown(sock_fd, SHUT_RDWR);
+
     close(sock_fd);
 
     return 0;
--- common.h
+++ common.h
@@ -1,45 +1,66 @@
+#ifndef COMMON_H
+#define COMMON_H
+#include <signal.h>
 #include <sys/socket.h>
 #include <unistd.h>
 #include <arpa/inet.h>
 #include <iostream>
+#include <cstdint>
 #include "check.hpp"
 
 constexpr unsigned short SERVER_PORT = 60002;
 constexpr int SOCKET_TYPE = SOCK_STREAM;
 
+enum Response {
+    LOWER = -1,
+    CORRECT = 0,
+    HIGHER = 1,
+};
+
 inline std::ostream& operator<<(std::ostream& s, const sockaddr_in& addr) {
     union {
         in_addr_t x;
-        char c[sizeof(in_addr)];
-    }t{};
+        unsigned char c[sizeof(in_addr)];
+    } t{};
+
     t.x = addr.sin_addr.s_addr;
-    return s << std::to_string(int(t.c[0]))
-    << "." << std::to_string(int(t.c[1]))
-    << "." << std::to_string(int(t.c[2]))
-    << "." << std::to_string(int(t.c[3]))
-    << ":" << std::to_string(ntohs(addr.sin_port));
+
+    return s
+        << std::to_string(t.c[0]) << "."
+        << std::to_string(t.c[1]) << "."
+        << std::to_string(t.c[2]) << "."
+        << std::to_string(t.c[3]) << ":"
+        << std::to_string(ntohs(addr.sin_port));
 }
 
-
 inline int make_socket(int type) {
-    switch (type)
-    {
+    switch (type) {
         case SOCK_STREAM:
             return socket(AF_INET, SOCK_STREAM, 0);
+
         case SOCK_SEQPACKET:
-            return check(socket(AF_INET, SOCK_STREAM, IPPROTO_SCTP)); // analogue to
-            SOCK_SEQPACKET;
-            default:
+            return check(socket(AF_INET, SOCK_STREAM, IPPROTO_SCTP));
+
+        default:
             errno = EINVAL;
             return -1;
     }
 }
 
-
 inline sockaddr_in local_addr(unsigned short port) {
     sockaddr_in addr{};
-    addr.sin_addr.s_addr = htonl(INADDR_LOOPBACK);//127.0.0.1
+
+    addr.sin_addr.s_addr = htonl(INADDR_LOOPBACK);
     addr.sin_port = htons(port);
     addr.sin_family = AF_INET;
+
     return addr;
 }
+
+inline void ignore_sigpipe() {
+    struct sigaction sa{};
+    sa.sa_handler = SIG_IGN;
+    sigaction(SIGPIPE, &sa, nullptr);
+}
+
+#endif
